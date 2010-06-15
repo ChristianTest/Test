ba86837fcec0d10a9f4f39783a9dce6e078c6ace
Christian Kraus <chrism-92@live.de>
2010-06-15 19:05:22 +0200

    HelloWorld.java erste Version.

---
+++ Test/Helloworld.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class Helloworld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Helloworld {
+    public static void main (String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
