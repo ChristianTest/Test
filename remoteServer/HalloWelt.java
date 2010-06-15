b7113d99f092a81f579d47a9c562df94d01398c1
Christian Kraus <chrism-92@live.de>
2010-06-15 18:05:18 +0200

    Nur Hallo Welt.
    Signed-off-by: Christian Kraus <chrism-92@live.de>

---
+++ remoteServer/HalloWelt.java
@@ -0,0 +1,12 @@
+
+/**
+ * Sage hallo zur Welt!
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class HalloWelt {
+    public static void main (String[] args) {
+        System.out.println("Hallo Welt");
+    }
+}
