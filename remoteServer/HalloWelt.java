53662d4a6a6d383dd76d623eae269731e40ea796
Christian Kraus <chrism-92@live.de>
2010-06-15 18:33:49 +0200

    Nur Hallo Welt

---
+++ remoteServer/HalloWelt.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class HalloWelt here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class HalloWelt {
+    public static void main (String[] args) {
+        System.out.println("Hallo Welt!");
+    }
+
+}
